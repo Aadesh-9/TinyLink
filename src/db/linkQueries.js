2995a5cb569ad8813dbdb0c06c10bda41c8c6f3f
Aadesh Wasamkar <aadeshwasamkar@gmail.com>
2025-11-22 16:52:48 +0530

    Added  SQL query for soft deleting a link

--- src/db/linkQueries.js
+++ src/db/linkQueries.js
@@ -50,10 +50,21 @@ const updateLastClicked = async (code) => {
   await db.query(q, [code]);
 };
 
+// Mark a link as deleted
+const markAsDeleted = async (code) => {
+  const q = `
+    UPDATE urls
+    SET is_deleted = TRUE
+    WHERE id = $1
+  `;
+  await db.query(q, [code]);
+};
+
 module.exports = {
   checkCodeExists,
   insertLink,
   findByCode,
   incrementClicks,
   updateLastClicked,
+  markAsDeleted,
 };
