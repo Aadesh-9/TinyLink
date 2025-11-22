9db4040c3369fc06be4fc9d6691f57e5bbea7dff
Aadesh Wasamkar <aadeshwasamkar@gmail.com>
2025-11-22 17:08:24 +0530

    Added files for  fetching all links

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,8 +2,12 @@
 const express = require("express");
 const router = express.Router();
 const { createLink } = require("../controllers/linksController");
+const { listLinks } = require("../controllers/listController");
 
 // POST /api/links
 router.post("/", createLink);
+
+// GET /api/links (list all links)
+router.get("/", listLinks);
 
 module.exports = router;
